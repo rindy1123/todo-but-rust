0610a141eaf71d0de16d51e38e54402a1c1a70c1
rindy1123 <genryusinbaou@gmail.com>
2023-05-07 16:39:02 -0700

    feat: update done

--- web/src/pages/todos/list.rs
+++ web/src/pages/todos/list.rs
@@ -1,6 +1,8 @@
 use std::ops::Deref;
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_router::prelude::use_navigator;
 
@@ -10,15 +12,27 @@ use crate::{constants::API_URL, Route};
 struct ToDoListResponse {
     todos: Vec<ToDo>,
 }
-#[derive(Deserialize, Default, Clone)]
+#[derive(Deserialize, Default, Clone, PartialEq)]
 struct ToDo {
     id: String,
     done: bool,
     description: String,
 }
 
-fn to_list_element(todo: ToDo) -> Html {
-    let cloned_todo = todo.clone();
+#[derive(Properties, PartialEq)]
+struct DeleteButtonProps {
+    todo: ToDo,
+}
+
+#[derive(Serialize)]
+pub struct PutToDo {
+    done: bool,
+    description: String,
+}
+
+#[function_component(DeleteButton)]
+fn delete_button(props: &DeleteButtonProps) -> Html {
+    let cloned_todo = props.todo.clone();
     let onclick = Callback::from(move |_| {
         let id = cloned_todo.id.clone();
         wasm_bindgen_futures::spawn_local(async move {
@@ -31,10 +45,41 @@ fn to_list_element(todo: ToDo) -> Html {
         });
     });
     html! {
-        <li>
-            {format!("id: {} | {} | {}", todo.id, todo.description, todo.done)}
-            <button {onclick}>{"delete"}</button>
-        </li>
+        <button {onclick}>{"delete"}</button>
+    }
+}
+
+fn to_list_element(todo: ToDo) -> Html {
+    let cloned_todo = todo.clone();
+    let onclick = Callback::from(move |e: MouseEvent| {
+        let target = e.target().unwrap();
+        let done = target.unchecked_into::<HtmlInputElement>().checked();
+        let id = cloned_todo.id.clone();
+        let description = cloned_todo.description.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let client = reqwest_wasm::Client::new();
+            let body = PutToDo { done, description };
+            client
+                .put(format!("{}/{}", API_URL, id))
+                .json(&body)
+                .send()
+                .await
+                .unwrap();
+        });
+    });
+
+    html! {
+        <div>
+            <input
+                type="checkbox"
+                id={todo.id.clone()}
+                name={todo.id.clone()}
+                checked={todo.done}
+                {onclick}
+            />
+            <label for={todo.id.clone()}>{todo.description.clone()}</label>
+            <DeleteButton {todo} />
+        </div>
     }
 }
 
@@ -60,11 +105,9 @@ fn list() -> Html {
     let todos = todo_list_response.deref().todos.clone().into_iter();
 
     html! {
-        <ul>
-            {
-                todos.map(to_list_element).collect::<Html>()
-            }
-        </ul>
+        {
+            todos.map(to_list_element).collect::<Html>()
+        }
     }
 }
 
